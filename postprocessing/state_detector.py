c0e701c8bbc47b96fa8449cbb8fb21c2906763c8
Hugh Gottlieb <h.gottlieb@unsw.edu.au>
2025-02-14 15:46:09 +1100

    Add basic state / transition detectors

---
+++ postprocessing/state_detector.py
@@ -0,0 +1,15 @@
+from dpl_common.helpers import Image, Transition
+from dpl_common.config import Config
+
+class StateDetector:
+    def __init__(self):
+        pass
+
+    # TODO - implement this properly
+    def tag_states(self, images: list[Image], transitions: list[Transition], config: Config):
+        assert (len(transitions) == 1), "StateDetector cannot handle multiple transitions yet"
+        for image in images:
+            if image.time < transitions[0].time:
+                image.pl_state = Image.PL_State.HIGH if transitions[0].direction == Transition.Direction.HIGH_TO_LOW else Image.PL_State.LOW
+            else:
+                image.pl_state = Image.PL_State.LOW if transitions[0].direction == Transition.Direction.HIGH_TO_LOW else Image.PL_State.HIGH
---
+++ postprocessing/transition_detector.py
@@ -0,0 +1,7 @@
+from dpl_common.helpers import Image, Transition
+
+# NOTE: at the moment this is a shell class, since all processed datasets should have a tagged transition
+
+class TransitionDetector:
+    def detect_transitions(self, images: list[Image]) -> list[Transition]:
+        raise NotImplementedError("detect_transition has not implemented")
