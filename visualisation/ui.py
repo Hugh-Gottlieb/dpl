a36132dfae83ac09a42adce88756190461089b05
Hugh Gottlieb <h.gottlieb@unsw.edu.au>
2025-02-19 17:17:40 +1100

    use consistent colour scale when zoom

--- visualisation/ui.py
+++ visualisation/ui.py
@@ -5,7 +5,6 @@ import sys
 import numpy as np
 import cv2
 
-from dpl_common.helpers import tif_to_jpeg
 from dpl_common.config import Config, get_config_path
 from dpl_common.mission import Mission
 
@@ -20,6 +19,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.mission = None
         self.display_scene = QGraphicsScene(self)
         self.display.setScene(self.display_scene)
+        self.zoom_limit_min, self.zoom_limit_max = None, None
 
     def set_mission(self):
         options = QFileDialog.Option.ShowDirsOnly | QFileDialog.Option.DontUseNativeDialog
@@ -29,6 +29,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.mission_path.setText(folder_path)
         self.mission = Mission(folder_path)
         self.status.setText(f"Loaded mission - {len(self.mission.get_acquisitions())} images")
+        self.zoom_limit_min, self.zoom_limit_max = None, None # Reset to None so dont use from previous mission
 
     def show_overview(self):
         if self.mission is None:
@@ -57,9 +58,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             cell_x2 = cell_x1 + x_size
             cell_y2 = cell_y1 + y_size
             overview_img[cell_y1:cell_y2, cell_x1:cell_x2] = img[:,:]
+        self.zoom_limit_min = np.nanpercentile(overview_img, 2.5)
+        self.zoom_limit_max = np.nanpercentile(overview_img, 97.5)
         nan_mask = np.isnan(overview_img)
         overview_img[nan_mask] = 0
-        overview_img = tif_to_jpeg(overview_img)
+        overview_img = self.__tif_to_jpeg(overview_img, self.zoom_limit_min, self.zoom_limit_max)
         overview_img[nan_mask] = [255, 255, 255]
         for i in range(len(self.mission.get_acquisitions())):
             row, col = self.__get_row_col(i, rows, cols)
@@ -67,6 +70,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             cell_y1 = space + row * (y_size + space) + y_size - 2 * space
             overview_img = cv2.putText(overview_img, str(i), (cell_x1, cell_y1), cv2.FONT_HERSHEY_SIMPLEX, 5, (50,205,50), 5, cv2.LINE_AA)
         return overview_img
+
+    # New tif to jpeg which uses a set limit, instead of a dynamic percentile
+    def __tif_to_jpeg(self, img, limit_min, limit_max):
+        img = img.astype(np.float32)
+        preview = (img - limit_min) / (limit_max - limit_min)
+        preview = (np.clip(preview, 0, 1) * (2**8 - 1)).astype(np.uint8)
+        return cv2.applyColorMap(preview, cv2.COLORMAP_INFERNO)
 
     # Snake, starting from bottom left
     def __get_row_col(self, i, rows, cols):
@@ -84,8 +94,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if zoom_id >= len(self.mission.get_acquisitions()):
             self.status.setText(f"Error: Mission has no image with ID {zoom_id}")
             return
+        if self.zoom_limit_min is None:
+            self.status.setText(f"Error: please display an overview before zooming")
+            return
         acq = self.mission.get_acquisitions()[zoom_id]
-        self.__display_img(tif_to_jpeg(acq.get_pl_image().data))
+        self.__display_img(self.__tif_to_jpeg(acq.get_pl_image().data, self.zoom_limit_min, self.zoom_limit_max))
         self.status.setText(f"Showing zoom of {acq.get_name()}")
 
     def __display_img(self, img):
